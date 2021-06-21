ad0791ef655326c9dc108ecb55fbd58e99ed81ca
Leslie <lesliezsy12@gmail.com>
2021-06-21 23:06:50 +0800

    add admin create restaurant feature

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -6,7 +6,28 @@ const adminController = {
     return Restaurant.findAll({ raw: true }).then(restaurants => {
       return res.render('admin/restaurants', { restaurants })
     })
-  }
+  },
+  createRestaurant: (req, res) => {
+    return res.render('admin/create')
+  },
+  postRestaurant: (req, res) => {
+    // 餐廳名稱為必填，沒填的話會導回去新增餐廳的頁面
+    if (!req.body.name) {
+      req.flash('error_messages', "Name is required.")
+      return res.redirect('back')
+    }
+    return Restaurant.create({
+        name: req.body.name,
+        tel: req.body.tel,
+        address: req.body.address,
+        opening_hours: req.body.opening_hours,
+        description: req.body.description
+      })
+      .then((restaurant) => {
+        req.flash('success_messages', 'Restaurant was successfully created.')
+        res.redirect('/admin/restaurants')
+      })
+  },
 }
 
 module.exports = adminController
