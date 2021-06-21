dcd4399aec519b3864403839722faa7bf35645db
Leslie <lesliezsy12@gmail.com>
2021-06-21 21:42:33 +0800

    add isAdmin to users & add authentication before rendering pages

--- routes/index.js
+++ routes/index.js
@@ -4,17 +4,34 @@ const userController = require('../controllers/userController.js')
 
 const passport = require('../config/passport')
 
-module.exports = app => {
+module.exports = (app, passport) => {
+  // 身份驗證 - 判斷是否登入
+  const authenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+    // 沒登入
+    res.redirect('/signin')
+  }
+  // 判斷是否具管理員身份
+  const authenticatedAdmin = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      if (req.user.isAdmin) { return next() }
+      return res.redirect('/')
+    }
+    // 沒登入
+    res.redirect('/signin')
+  }
 
   //如果使用者訪問首頁，就導向 /restaurants 的頁面
-  app.get('/', (req, res) => res.redirect('/restaurants'))
+  app.get('/', authenticated, (req, res) => res.redirect('/restaurants'))
   //在 /restaurants 底下則交給 restController.getRestaurants 來處理
-  app.get('/restaurants', restController.getRestaurants)
+  app.get('/restaurants', authenticated, restController.getRestaurants)
 
   // 連到 /admin 頁面就轉到 /admin/restaurants
-  app.get('/admin', (req, res) => res.redirect('/admin/restaurants'))
+  app.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/restaurants'))
   // 在 /admin/restaurants 底下則交給 adminController.getRestaurants 處理
-  app.get('/admin/restaurants', adminController.getRestaurants)
+  app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
 
   app.get('/signup', userController.signUpPage)
   app.post('/signup', userController.signUp)
