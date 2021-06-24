4e91a4ddfecab0286993468414abde8067e6b341
Leslie <lesliezsy12@gmail.com>
2021-06-25 00:17:18 +0800

    add categories navbar on restaurants page

--- controllers/restController.js
+++ controllers/restController.js
@@ -5,17 +5,34 @@ const { Restaurant, Category } = db
 const restController = {
   // 瀏覽餐廳頁面
   getRestaurants: (req, res) => {
-    Restaurant.findAll({ include: Category }).then(restaurants => {
-      const data = restaurants.map(restaurant => ({
-        ...restaurant.dataValues,
-        description: restaurant.dataValues.description.substring(0, 50),
-        categoryName: restaurant.Category.name
-      }))
-      // restaurant.Category.name 可以這樣用是因為在 restaurant model 裡有設關聯性
-      return res.render('restaurants', {
-        restaurants: data
+    const whereQuery = {}
+    let categoryId = ''
+    // 如果前端傳來 category id
+    if (req.query.categoryId) {
+      categoryId = Number(req.query.categoryId)
+      whereQuery.CategoryId = categoryId
+    }
+
+    Restaurant.findAll(({ raw: true, nest: true, include: Category, where: whereQuery }))
+      .then(restaurants => {
+        const data = restaurants.map(restaurant => ({
+          ...restaurant,
+          description: restaurant.description.substring(0, 50),
+          categoryName: restaurant.Category.name
+        }))
+        // restaurant.Category.name 可以這樣用是因為在 restaurant model 裡有設關聯性
+        Category.findAll({
+          raw: true,
+          nest: true
+        }).then(categories => { // 取出 categoies 
+          return res.render('restaurants', {
+            restaurants: data,
+            categories,
+            categoryId
+          })
+        })
+
       })
-    })
   },
   getRestaurant: (req, res) => {
     return Restaurant.findByPk(req.params.id, {
