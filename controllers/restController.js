4f795439b6dc2a364e419ccf15edb8f74fc790f7
Leslie <lesliezsy12@gmail.com>
2021-06-25 16:25:31 +0800

    add pagination on restaurants

--- controllers/restController.js
+++ controllers/restController.js
@@ -2,20 +2,45 @@
 const db = require('../models')
 const { Restaurant, Category } = db
 
+const pageLimit = 10 // 一頁有 10 筆資料
+
 const restController = {
   // 瀏覽餐廳頁面
   getRestaurants: (req, res) => {
+    let offset = 0 // 從哪開始取資料
     const whereQuery = {}
     let categoryId = ''
+
+    // 若前端傳來 page 資訊
+    if (req.query.page) {
+      offset = (req.query.page - 1) * pageLimit
+    }
     // 如果前端傳來 category id
     if (req.query.categoryId) {
       categoryId = Number(req.query.categoryId)
       whereQuery.CategoryId = categoryId
     }
 
-    Restaurant.findAll(({ raw: true, nest: true, include: Category, where: whereQuery }))
-      .then(restaurants => {
-        const data = restaurants.map(restaurant => ({
+    Restaurant.findAndCountAll({
+        raw: true,
+        nest: true,
+        include: Category,
+        where: whereQuery,
+        offset: offset,
+        limit: pageLimit
+      })
+      .then(result => {
+        // data for pagination
+
+        // 第一次進入首頁時，沒帶任何參數，req.query.page 會回傳 undefined
+        // 如果 || 左邊的運算結果是 false 或 undefined，就會拿到 || 右邊的值
+        const page = Number(req.query.page) || 1
+        const pages = Math.ceil(result.count / pageLimit) // 總共有幾頁
+        const totalPage = Array.from({ length: pages }).map((item, index) => index + 1)
+        const prev = page - 1 < 1 ? 1 : page - 1
+        const next = page + 1 > pages ? pages : page + 1
+        // result.rows 是所有餐廳資料
+        const data = result.rows.map(restaurant => ({
           ...restaurant,
           description: restaurant.description.substring(0, 50),
           categoryName: restaurant.Category.name
@@ -28,7 +53,11 @@ const restController = {
           return res.render('restaurants', {
             restaurants: data,
             categories,
-            categoryId
+            categoryId,
+            page,
+            totalPage,
+            prev,
+            next
           })
         })
 
