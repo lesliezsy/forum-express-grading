702ab8538b7f10cb2ed7dae99f7b2ccc0eb66367
Leslie <lesliezsy12@gmail.com>
2021-06-26 12:34:31 +0800

    create favorite model and set association between restaurant and user

--- models/restaurant.js
+++ models/restaurant.js
@@ -10,8 +10,18 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
+      // 一家餐廳只屬於一個category，一個category可有很多餐廳: 1 to 1 - 1 to M
       Restaurant.belongsTo(models.Category)
+      // 一間餐廳有很多評論，但一則評論只屬於一人: 1 to M - 1 to 1
       Restaurant.hasMany(models.Comment)
+      // 一間餐廳可以被很多User加到最愛，一個user可有很多最愛的餐廳: 1 to M - 1 to M
+      // Rest & User: 中間需透過 Favorite 這張 table 查找關係
+      // belongsToMany 表示這是一個多對多關係
+      Restaurant.belongsToMany(models.User, {
+        through: models.Favorite, // 透過 favorite table去查找餐廳與user之關聯
+        foreignKey: 'RestaurantId', // 透過固定 RestaurantId 這欄位去取得 User 內符合條件的users (會拿到和這間餐廳有關係的 UserId)
+        as: 'FavoritedUsers' // 改 RestaurantId 稱為 FavoritedUsers
+      })
     }
   };
   Restaurant.init({
