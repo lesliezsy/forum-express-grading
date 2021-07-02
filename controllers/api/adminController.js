c0dbe5b9f5f62ca2ba2b589cd98de858e020c19a
Leslie <lesliezsy12@gmail.com>
2021-07-02 18:02:19 +0800

    remove redundant code

--- controllers/api/adminController.js
+++ controllers/api/adminController.js
@@ -10,11 +10,6 @@ const adminController = {
     adminService.getRestaurant(req, res, (data) => {
       return res.json(data)
     })
-  },
-  getCategories: (req, res) => {
-    adminService.getCategories(req, res, (data) => {
-      return res.json(data)
-    })
-  },
+  }
 }
 module.exports = adminController
