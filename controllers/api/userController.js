f86e0ecf67da158641a8bb161ff285914839edc8
Leslie <lesliezsy12@gmail.com>
2021-07-02 22:45:34 +0800

    add JWT api signin

---
+++ controllers/api/userController.js
@@ -0,0 +1,44 @@
+const bcrypt = require('bcryptjs')
+const db = require('../../models')
+const { User } = db
+
+// JWT
+const jwt = require('jsonwebtoken')
+const passportJWT = require('passport-jwt')
+const { ExtractJwt, JwtStrategy } = passportJWT
+
+const userController = {
+  signIn: (req, res) => {
+
+    let { email: username, password } = req.body
+
+    // 檢查必要資料
+    if (!username || !password) {
+      return res.json({ status: 'error', message: "Required fields didn't exist" })
+    }
+    // 檢查 user 是否存在與密碼是否正確
+    User.findOne({ where: { email: username } }).then(user => {
+      if (!user) return res.status(401).json({ status: 'error', message: 'No such user found' })
+      if (!bcrypt.compareSync(password, user.password)) {
+        return res.status(401).json({ status: 'error', message: 'Passwords did not match' })
+      }
+      // 簽發 token
+      var payload = { id: user.id }
+      // jwt.sign(): 將資料轉為 JSON web token
+      var token = jwt.sign(payload, process.env.JWT_SECRET)
+      return res.json({
+        status: 'success',
+        message: 'ok',
+        token,
+        user: {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          isAdmin: user.isAdmin
+        }
+      })
+    })
+  }
+}
+
+module.exports = userController
--- routes/apis.js
+++ routes/apis.js
@@ -6,12 +6,15 @@ const upload = multer({ dest: 'temp/' })
 
 const adminController = require('../controllers/api/adminController.js')
 const categoryController = require('../controllers/api/categoryController.js')
+const userController = require('../controllers/api/userController.js')
+
+// JWT signin
+router.post('/signin', userController.signIn)
 
 router.get('/admin/restaurants', adminController.getRestaurants)
 router.get('/admin/restaurants/:id', adminController.getRestaurant)
 router.delete('/admin/restaurants/:id', adminController.deleteRestaurant)
 router.post('/admin/restaurants', upload.single('image'), adminController.postRestaurant)
-
 
 router.get('/admin/categories', categoryController.getCategories)
 
